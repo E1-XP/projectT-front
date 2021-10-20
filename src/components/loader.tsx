5b171e85e5c8eb59d1a26ed75d461860da76d8c1
E1-XP <poczta.elektroniczna@hotmail.com>
2021-10-20 21:44:07 +0200

    add loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,47 @@
+import React, { PropsWithChildren } from "react";
+import styled, { keyframes } from "styled-components";
+import { useStoreSelector } from "../hooks";
+
+const Main_preloader = styled.div`
+  background-color: #333;
+  color: white;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+  height: 100vh;
+`;
+
+const rotateAnim = keyframes`
+    from{
+        transform:rotate(0deg);
+    }
+    to{
+        transform:rotate(360deg);
+    }
+`;
+
+const Spinner = styled.div`
+  margin-top: 1rem;
+  padding: 0.6rem;
+  border: 3px solid red;
+  border-right: 3px solid transparent;
+  border-radius: 50%;
+  transform: translateZ(0);
+  animation: ${rotateAnim} 0.5s linear infinite;
+`;
+
+export const Loader = (props: PropsWithChildren<{}>) => {
+  const isLoading = useStoreSelector((state) => state.global.isLoading);
+
+  return isLoading ? (
+    <Main_preloader>
+      <h1>ProjectT</h1>
+      <Spinner />
+    </Main_preloader>
+  ) : (
+    <> {props.children}</>
+  );
+};
