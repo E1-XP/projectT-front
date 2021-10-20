54374406e9011c0d650eb193ba635eecbc128b7e
E1-XP <poczta.elektroniczna@hotmail.com>
2021-10-20 15:20:12 +0200

    add css links to page head using react-helmet

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 
+import { Head } from "./head";
 import { Routes } from "./routes";
 import { history } from "./routes/history";
 import { store } from "./store";
 import { GlobalStyle } from "./styles";
 
 export const App = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <GlobalStyle />
-      <Routes />
-    </ConnectedRouter>
-  </Provider>
+  <>
+    <Head />
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <GlobalStyle />
+        <Routes />
+      </ConnectedRouter>
+    </Provider>
+  </>
 );
---
+++ src/head.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export const Head = () => (
+  <Helmet>
+    <title>ProjectT</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&display=swap"
+      rel="stylesheet"
+    ></link>
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    ></link>
+  </Helmet>
+);
