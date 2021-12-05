d9046fe15011a83207f8b1be91f2e149ce92d1e0
E1-XP <poczta.elektroniczna@hotmail.com>
2021-12-05 22:24:35 +0100

    simplify entry grouping components

--- src/pages/timer/dayList.tsx
+++ src/pages/timer/dayList.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useReducer, Dispatch } from "react";
+import React from "react";
 import styled from "styled-components";
 import format from "date-fns/format";
 import intervalToDuration from "date-fns/intervalToDuration";
-import cloneDeep from "lodash/fp/cloneDeep";
 
 import {
   GroupedEntries,
@@ -10,9 +9,8 @@ import {
 } from "./../../selectors/groupEntriesByDays";
 import { Entry as IEntry } from "./../../store/interfaces";
 
-import { Entry } from "./entry";
 import { formatDuration } from "./../../helpers";
-import { whiteGrey } from "../../styles/variables";
+import { EntryGroup } from "./entryGroup";
 
 interface Props {
   data: SingleDay;
@@ -37,68 +35,51 @@ const Header_dayCount = styled.span`
 
 const Project_list = styled.ul``;
 
-const Project_item = styled.li`
-  display: flex;
-  justify-content: space-between;
-  padding: 1rem 0;
-  margin: auto 0;
-  align-items: center;
-
-  &:hover {
-    background-color: ${whiteGrey};
-  }
-`;
-
-const Entry_list = styled.ul`
-  width: 100%;
-`;
-
-type EntryListDisplayState = Record<string, boolean>;
 type GroupedEntriesSorted = GroupedEntries & {
   entriesByDescription: IEntry[][];
 };
 
-interface Action {
-  type: string;
-  payload: boolean;
-}
+const getEntriesSortedByDescription = (entries: IEntry[]) =>
+  entries.reduce((acc, entry) => {
+    const arrIdxCandidate = acc.findIndex(
+      (innerArr) => innerArr[0].description === entry.description
+    );
+    if (arrIdxCandidate === -1) acc.push([]);
+    const arrIdx = arrIdxCandidate >= 0 ? arrIdxCandidate : acc.length - 1;
 
-const setDisplayListAction = (type: string, payload: boolean) => ({
-  type,
-  payload,
-});
+    acc[arrIdx].push(entry);
 
-const displayStateReducer = (state: EntryListDisplayState, action: Action) => ({
-  ...state,
-  [action.type]: action.payload,
-});
+    return acc;
+  }, [] as IEntry[][]);
 
-const projectStateReducer = (
-  state: Record<string, EntryListDisplayState>,
-  action: Action
-) => {
-  const [project, description] = action.type.split("@@");
-
-  const updatedState = cloneDeep(state);
-  updatedState[project][description] = action.payload;
-
-  return updatedState;
-};
-
-export const DayList = ({ data }: Props) => {
-  const getEntriesSortedByDescription = (entries: IEntry[]) =>
-    entries.reduce((acc, entry) => {
-      const arrIdxCandidate = acc.findIndex(
-        (innerArr) => innerArr[0].description === entry.description
-      );
-      if (arrIdxCandidate === -1) acc.push([]);
-      const arrIdx = arrIdxCandidate >= 0 ? arrIdxCandidate : acc.length - 1;
-
-      acc[arrIdx].push(entry);
+const getStartAndStopDates = (entries: IEntry[]) =>
+  entries.reduce(
+    (acc, entry) => {
+      if (entry.start < acc.start) acc.start = entry.start;
+      if (entry.stop > acc.stop) acc.stop = entry.stop;
 
       return acc;
-    }, [] as IEntry[][]);
+    },
+    {
+      start: entries.length ? entries[0].start : 0,
+      stop: entries.length ? entries[0].stop : 0,
+    }
+  );
 
+const getHeaderData = (entriesArr: IEntry[][]) =>
+  entriesArr.reduce((acc, entries) => {
+    acc.push({
+      entries,
+      totalDuration: entries.reduce(
+        (acc, entry) => acc + (entry.stop - entry.start),
+        0
+      ),
+      ...getStartAndStopDates(entries),
+    });
+    return acc;
+  }, [] as GroupedEntries[]);
+
+export const DayList = ({ data }: Props) => {
   const entriesWithoutProject = getEntriesSortedByDescription(
     data.entries.filter((entry) => !entry.project)
   );
@@ -114,33 +95,6 @@ export const DayList = ({ data }: Props) => {
     {} as Record<string, GroupedEntriesSorted>
   );
 
-  const getStartAndStopDates = (entries: IEntry[]) =>
-    entries.reduce(
-      (acc, entry) => {
-        if (entry.start < acc.start) acc.start = entry.start;
-        if (entry.stop > acc.stop) acc.stop = entry.stop;
-
-        return acc;
-      },
-      {
-        start: entries.length ? entries[0].start : 0,
-        stop: entries.length ? entries[0].stop : 0,
-      }
-    );
-
-  const getHeaderData = (entriesArr: IEntry[][]) =>
-    entriesArr.reduce((acc, entries) => {
-      acc.push({
-        entries,
-        totalDuration: entries.reduce(
-          (acc, entry) => acc + (entry.stop - entry.start),
-          0
-        ),
-        ...getStartAndStopDates(entries),
-      });
-      return acc;
-    }, [] as GroupedEntries[]);
-
   const entriesWithoutProjectHeaderData = getHeaderData(entriesWithoutProject);
   const projectEntriesHeaderData = Object.entries(projectEntries).reduce(
     (acc, [key, value]) => {
@@ -148,35 +102,6 @@ export const DayList = ({ data }: Props) => {
       return acc;
     },
     {} as Record<string, GroupedEntries[]>
-  );
-
-  const initNoProjectState = Object.values(
-    entriesWithoutProjectHeaderData
-  ).reduce((acc, data) => {
-    acc[data.entries[0].description] = false;
-    return acc;
-  }, {} as EntryListDisplayState);
-
-  const [noProjectState, dispatch] = useReducer(
-    displayStateReducer,
-    initNoProjectState
-  );
-
-  const initialProjectState = Object.entries(projectEntriesHeaderData).reduce(
-    (acc, [key, groupedArr]) => {
-      acc[key] = groupedArr.reduce((acc, grouped) => {
-        acc[grouped.entries[0].description] = false;
-        return acc;
-      }, {} as EntryListDisplayState);
-
-      return acc;
-    },
-    {} as Record<string, EntryListDisplayState>
-  );
-
-  const [projectDisplayState, projectDisplayDispatch] = useReducer(
-    projectStateReducer,
-    initialProjectState
   );
 
   return (
@@ -190,69 +115,12 @@ export const DayList = ({ data }: Props) => {
         </Header_dayCount>
       </Header>
       <Project_list>
-        {/*append project entries */}
-        {Object.entries(projectEntriesHeaderData).map(
-          ([projectKey, groupedArr]) =>
-            groupedArr.map((grouped) => (
-              <Project_item key={grouped.start}>
-                <Entry_list>
-                  {grouped.entries.length > 1 && (
-                    <Entry
-                      asEntryHeader={true}
-                      size={grouped.entries.length}
-                      data={grouped}
-                      isOpen={
-                        projectDisplayState[projectKey][
-                          grouped.entries[0].description
-                        ]
-                      }
-                      setIsOpen={(v) =>
-                        projectDisplayDispatch(
-                          setDisplayListAction(
-                            `${projectKey}@@${grouped.entries[0].description}`,
-                            v
-                          )
-                        )
-                      }
-                    />
-                  )}
-                  {(projectDisplayState[projectKey][
-                    grouped.entries[0].description
-                  ] ||
-                    grouped.entries.length === 1) &&
-                    grouped.entries.map((entry) => (
-                      <Entry key={entry._id} data={entry} />
-                    ))}
-                </Entry_list>
-              </Project_item>
-            ))
+        {Object.values(projectEntriesHeaderData).map((groupedArr) =>
+          groupedArr.map((data) => <EntryGroup key={data.start} data={data} />)
         )}
-        {/*append entries without project */}
-        {!!entriesWithoutProject.length &&
-          entriesWithoutProjectHeaderData.map((data) => (
-            <Project_item key={data.entries[0].start}>
-              <Entry_list>
-                {data.entries.length > 1 && (
-                  <Entry
-                    asEntryHeader={true}
-                    size={data.entries.length}
-                    data={data}
-                    isOpen={noProjectState[data.entries[0].description]}
-                    setIsOpen={(v) =>
-                      dispatch(
-                        setDisplayListAction(data.entries[0].description, v)
-                      )
-                    }
-                  />
-                )}
-                {(noProjectState[data.entries[0].description] ||
-                  data.entries.length === 1) &&
-                  data.entries.map((entry) => (
-                    <Entry key={entry._id} data={entry} />
-                  ))}
-              </Entry_list>
-            </Project_item>
-          ))}
+        {Object.values(entriesWithoutProjectHeaderData).map((data) => (
+          <EntryGroup key={data.start} data={data} />
+        ))}
       </Project_list>
     </>
   );
---
+++ src/pages/timer/entryGroup.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+
+import { GroupedEntries } from "./../../selectors/groupEntriesByDays";
+import { whiteGrey } from "../../styles/variables";
+
+import { Entry } from "./entry";
+
+interface Props {
+  data: GroupedEntries;
+}
+
+const Project_item = styled.li`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 0;
+  margin: auto 0;
+  align-items: center;
+
+  &:hover {
+    background-color: ${whiteGrey};
+  }
+`;
+
+const Entry_list = styled.ul`
+  width: 100%;
+`;
+
+export const EntryGroup = ({ data }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const setIsOpenCB = useCallback(() => setIsOpen(!isOpen), [isOpen]);
+
+  return (
+    <Project_item key={data.start}>
+      <Entry_list>
+        {data.entries.length > 1 && (
+          <Entry
+            asEntryHeader={true}
+            size={data.entries.length}
+            data={data}
+            isOpen={isOpen}
+            setIsOpen={setIsOpenCB}
+          />
+        )}
+        {(isOpen || data.entries.length === 1) &&
+          data.entries.map((entry) => <Entry key={entry._id} data={entry} />)}
+      </Entry_list>
+    </Project_item>
+  );
+};
