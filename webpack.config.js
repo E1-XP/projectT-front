e4f730b87423ba33c6f103e5efb0f955a381fc0b
E1-XP <poczta.elektroniczna@hotmail.com>
2022-01-30 22:35:47 +0100

    handle image file imports

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ["file-loader"],
+      },
     ],
   },
   resolve: {
