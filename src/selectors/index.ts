aafb570e431ab8034c85b91e4129e3b14643ac95
E1-XP <poczta.elektroniczna@hotmail.com>
2022-04-10 15:31:20 +0200

    add more selectors

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -2,6 +2,8 @@ import { RootState } from "../store";
 
 export const selectUserData = (state: RootState) => state.user.userData;
 
+export const selectEntries = (state: RootState) => state.user.entries;
+
 export const selectTimer = (state: RootState) => state.timer;
 
-export const selectEntries = (state: RootState) => state.user.entries;
+export const selectGlobal = (state: RootState) => state.global;
