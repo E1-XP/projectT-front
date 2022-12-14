4d742ed481bb72178153b7d1b3109bf11f78eddd
E1-XP <poczta.elektroniczna@hotmail.com>
2022-12-14 17:51:27 +0100

    update api url

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const config = {
   API_URL:
     process.env.NODE_ENV === "production"
       ? `${window.location.origin}/api`
-      : "https://project-t.gtxcodeworks.site",
-  API_URL_ORIGIN: "https://project-t.gtxcodeworks.site",
+      : "https://project-t.gtxcodeworks.online",
+  API_URL_ORIGIN: "https://project-t.gtxcodeworks.online",
 };
